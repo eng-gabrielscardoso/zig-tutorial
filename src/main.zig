6ec4b5e4d3fb15bcd2de035e05a889eaed29ac9a
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-08-23 16:29:56 -0300

    array and slices

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const user = @import("models/user.zig");
 const User = user.User;
 const MAX_POWER = user.MAX_POWER;
-const expect = std.testing.expect;
 
 pub fn main() void {
     const goku = User{
@@ -14,4 +13,23 @@ pub fn main() void {
 
     User.diagnose(goku);
     goku.diagnose();
+
+    std.debug.print("Listing elements from: a\n", .{});
+    for (a) |elem| {
+        std.debug.print("elem: {}\n", .{elem});
+    }
+
+    std.debug.print("Listing elements from: b\n", .{});
+    for (b) |elem| {
+        std.debug.print("elem: {}\n", .{elem});
+    }
+
+    std.debug.print("Listing elements from: c\n", .{});
+    for (c) |elem| {
+        std.debug.print("elem: {}\n", .{elem});
+    }
 }
+
+const a = [5]i32{ 1, 2, 3, 4, 5 };
+const b = [5]i32{ 1, 2, 3, 4, 5 };
+const c = [_]i32{ 1, 2, 3, 4, 5 };
