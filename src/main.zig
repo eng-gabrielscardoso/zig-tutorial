a62fd7c3f4d39aa6dc8dfd81b371053d92a6c0c2
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-08-23 16:13:39 -0300

    using modules

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const User = @import("models/user.zig").User;
 const expect = std.testing.expect;
 
 pub fn main() void {
@@ -9,8 +10,3 @@ pub fn main() void {
 
     std.debug.print("{s}'s power is {d}.", .{ user.name, user.power });
 }
-
-pub const User = struct {
-    power: u64,
-    name: []const u8,
-};
