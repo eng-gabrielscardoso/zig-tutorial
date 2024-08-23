09b18086e79839fb1705957678d188b79f17490c
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-08-23 16:12:20 -0300

    more structured example

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,15 @@ const std = @import("std");
 const expect = std.testing.expect;
 
 pub fn main() void {
-    std.debug.print("Hello, {s}!\n", .{"World"});
+    const user = User{
+        .power = 9001,
+        .name = "Goku",
+    };
+
+    std.debug.print("{s}'s power is {d}.", .{ user.name, user.power });
 }
 
-test "always succeeds" {
-    try expect(true);
-}
-
-test "always fails" {
-    try expect(false);
-}
+pub const User = struct {
+    power: u64,
+    name: []const u8,
+};
